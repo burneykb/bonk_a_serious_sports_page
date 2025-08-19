d1345d736c314196ecd9f079e601a5a6c9ea8dc3
burneykb <burneykb@gmail.com>
2025-08-19 12:23:22 -0500

    :Adding logo

--- flask-pdf-collection/config.py
+++ flask-pdf-collection/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    PDF_FOLDER = os.path.join(BASE_DIR, 'app', 'pdfs')
+    PDF_FOLDER = os.path.join(BASE_DIR, 'app', 'static', 'pdfs')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Limit upload size to 16 MB
     ALLOWED_EXTENSIONS = {'pdf'}
