f35412e5bae20e85205c6016877679fd6851c41d
burneykb <burneykb@gmail.com>
2025-08-19 21:58:04 -0500

    correcting github actions

--- app/config.py
+++ app/config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    PDF_FOLDER = os.path.join(BASE_DIR, 'app', 'static', 'pdfs')
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
+    PDF_FOLDER = os.path.join(BASE_DIR, 'static', 'pdfs')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Limit upload size to 16 MB
     ALLOWED_EXTENSIONS = {'pdf'}
